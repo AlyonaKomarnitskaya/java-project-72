4f4988f837c48aa860c50e92a18fbc866bab67d2
Alena Komarnitskaya <mixinny1@gmail.com>
2022-12-19 22:15:03 +0500

    Ready to deploy

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,67 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+
+public final class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.valueOf(port);
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+    private static void addRoutes(Javalin app) {
+        app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start();
+    }
+
+    public static Javalin getApp() {
+
+        // Создаём приложение
+        Javalin app = Javalin.create(config -> {
+            // Включаем логгирование
+            config.enableDevLogging();
+            // Подключаем настроенный шаблонизатор к фреймворку
+            JavalinThymeleaf.configure(getTemplateEngine());
+        });
+
+        // Добавляем маршруты в приложение
+        addRoutes(app);
+
+        // Обработчик before запускается перед каждым запросом
+        // Устанавливаем атрибут ctx для запросов
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
